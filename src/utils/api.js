104a350f78a57d59410b0d1daf44d385e6f3f2d0
llama <1959202057@qq.com>
2021-02-24 13:10:56 +0800

    feat:解决unsafe

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { BASE_URL } from './url.js'
 
+console.log('api',BASE_URL)
 // 创建axios的配置文件，里面配置baseURL路径
 const config = {
-    // baseURL: BASE_URL
-    baseURL: 'http://localhost:8080'
+    baseURL: BASE_URL
+    // baseURL: 'http://localhost:8080'
 }
+console.log('api2',BASE_URL)
 
 // 根据create 方法来构建axios对象
 const API = axios.create(config)
